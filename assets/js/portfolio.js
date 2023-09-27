fbc65e328635598a487ac810dd065acf804c37db
Keven Mingoranze <keven.ogi@hotmail.com>
2023-09-27 17:33:30 -0300

    adicionando fotos do portfolio

--- assets/js/portfolio.js
+++
@@ -1,8 +0,0 @@
-const portfolioJSON = [
-    {name:'Emissao de RRT e projetos'},
-    {name:'Porcelanatos e Revestimentos', bg:'./assets/images/portfolio/a.jpeg', },
-    {name:'Forros e sancas', bg:'./assets/images/portfolio/b.jpeg'},
-    {name:'Instalaçoes elétricas e hidráulicas', bg:'./assets/images/portfolio/c.jpeg'},
-    {name:'Pinturas e efeitos', bg:'./assets/images/portfolio/d.jpeg'},
-    {name:'Pocelanataria', bg:'./assets/images/portfolio/e.jpeg'},
-];
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,31 +7,120 @@ service.map((item, index)=>{
     document.querySelector('#serviceArea').append(serviceItem);
 }); 
 
-portfolioJSON.map((item, index)=>{
-    let portfolioItem = document.querySelector('#portfolio').cloneNode(true);
+const projetos = '../assets/images/portfolio/emissao_rrt_projetos'; 
+
+fetch(projetos)
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(text, 'text/html');
+    const links = htmlDoc.querySelectorAll('a');
+    const imagens = Array.from(links).filter(link => {
+      return /\.(jpg|jpeg|png)$/.test(link.href);
+    });
+    const arrayDeImagens = imagens.map(imagem => imagem.href);
     
-    portfolioItem.querySelector('h2').innerHTML = item.name;
-    portfolioItem.querySelector('#portfolioImg').style.backgroundImage = `url('${item.bg}')`;
-    portfolioItem.addEventListener('click', (e)=>{
-        e.preventDefault()
-        let modal = document.querySelector('#modal');
-        modal.style.display = 'flex';
+    const areaDeImagens = document.getElementById('projetosArea');
+    arrayDeImagens.forEach(caminho => {
+        const imagem = document.createElement('img');
+        imagem.src = caminho;
+        imagem.alt = 'Descrição da imagem'; // Adicione uma descrição apropriada
+      
+        areaDeImagens.appendChild(imagem);
+      });
+  });
+
+  const porcelanatosRevestimentos = '../assets/images/portfolio/porcelanatos_e_revestimentos'; 
+
+fetch(porcelanatosRevestimentos)
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(text, 'text/html');
+    const links = htmlDoc.querySelectorAll('a');
+    const imagens = Array.from(links).filter(link => {
+      return /\.(jpg|jpeg|png)$/.test(link.href);
     });
-    document.querySelector('#closeModal').addEventListener('click', (e)=>{
-        e.preventDefault()
-        modal.style.display = 'none'
+    const arrayDeImagens = imagens.map(imagem => imagem.href);
+    
+    const areaDeImagens = document.getElementById('porcelanatos_revestimentosArea');
+    arrayDeImagens.forEach(caminho => {
+        const imagem = document.createElement('img');
+        imagem.src = caminho;
+        imagem.alt = 'Descrição da imagem'; // Adicione uma descrição apropriada
+      
+        areaDeImagens.appendChild(imagem);
+      });
+  });
+
+  const forros_sancas = '../assets/images/portfolio/forros_e_sancas'; 
+
+  fetch(forros_sancas)
+    .then(response => response.text())
+    .then(text => {
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(text, 'text/html');
+      const links = htmlDoc.querySelectorAll('a');
+      const imagens = Array.from(links).filter(link => {
+        return /\.(jpg|jpeg|png)$/.test(link.href);
+      });
+      const arrayDeImagens = imagens.map(imagem => imagem.href);
+      
+      const areaDeImagens = document.getElementById('forros_sancasArea');
+      arrayDeImagens.forEach(caminho => {
+          const imagem = document.createElement('img');
+          imagem.src = caminho;
+          imagem.alt = 'Descrição da imagem'; // Adicione uma descrição apropriada
+        
+          areaDeImagens.appendChild(imagem);
+        });
     });
-    document.querySelector('#portfolioArea').append(portfolioItem);
-})
 
-document.querySelector('#close').addEventListener('click', ()=>{
-    document.querySelector('#menuOpen').style.transform = 'translateY(-40rem)'
-});
+    const pinturas_efeitos = '../assets/images/portfolio/pinturas_e_efeitos'; 
 
-document.querySelector('#open').addEventListener('click', ()=>{
-    document.querySelector('#menuOpen').style.transform = 'translate(0)';
-});
+    fetch(pinturas_efeitos)
+      .then(response => response.text())
+      .then(text => {
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(text, 'text/html');
+        const links = htmlDoc.querySelectorAll('a');
+        const imagens = Array.from(links).filter(link => {
+          return /\.(jpg|jpeg|png)$/.test(link.href);
+        });
+        const arrayDeImagens = imagens.map(imagem => imagem.href);
+        
+        const areaDeImagens = document.getElementById('pinturas_efeitosArea');
+        arrayDeImagens.forEach(caminho => {
+            const imagem = document.createElement('img');
+            imagem.src = caminho;
+            imagem.alt = 'Descrição da imagem'; // Adicione uma descrição apropriada
+          
+            areaDeImagens.appendChild(imagem);
+          });
+      });
+  
 
-let slidesImg = document.querySelectorAll('.fotoPort').length;
-let larguraSlider = document.querySelector('#slider').clientWidth;
-document.querySelector('#sliderArea').style.widht = `${larguraSlider * slidesImg}`
+
+const pocelanataria = '../assets/images/portfolio/pocelanataria'; 
+
+fetch(pocelanataria)
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const htmlDoc = parser.parseFromString(text, 'text/html');
+    const links = htmlDoc.querySelectorAll('a');
+    const imagens = Array.from(links).filter(link => {
+      return /\.(jpg|jpeg|png)$/.test(link.href);
+    });
+    const arrayDeImagens = imagens.map(imagem => imagem.href);
+    
+    const areaDeImagens = document.getElementById('pocelanatariaArea');
+    arrayDeImagens.forEach(caminho => {
+        const imagem = document.createElement('img');
+        imagem.src = caminho;
+        imagem.alt = 'Descrição da imagem'; // Adicione uma descrição apropriada
+      
+        areaDeImagens.appendChild(imagem);
+      });
+  });
+
